c9c40967f0f0e67eca5b2fc61f7fee2e0e6cfe6b
ma34s <ma34s.sss@gmail.com>
2014-04-26 17:10:49 +0900

    LGL22(Optimus G2)_Android_JB_LGL2210a_Kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Nov 26 15:00:25 KST 2013"
+#define LINUX_COMPILE_BY "joonsuk.ryu"
+#define LINUX_COMPILE_HOST "LGEARND6B7"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
