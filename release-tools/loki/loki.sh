713facfc3d7855e0daf89d6efc678caf9d2c3a98
ma34s <ma34s.sss@gmail.com>
2014-04-30 00:16:04 +0900

    update loki binaries

--- release-tools/loki/loki.sh
+++ release-tools/loki/loki.sh
@@ -11,8 +11,8 @@ image=$1
 
 mkdir -p $C
 dd if=/dev/block/platform/msm_sdcc.1/by-name/aboot of=$C/aboot.img
-/tmp/loki/loki_patch $image $C/aboot.img /tmp/$image.img $C/$image.lok || exit 1
-/tmp/loki/loki_flash $image $C/$image.lok || exit 1
+/tmp/loki/loki_tool patch $image $C/aboot.img /tmp/$image.img $C/$image.lok || exit 1
+/tmp/loki/loki_tool flash $image $C/$image.lok || exit 1
 rm -rf $C
 rm -rf /tmp/loki
 exit 0
